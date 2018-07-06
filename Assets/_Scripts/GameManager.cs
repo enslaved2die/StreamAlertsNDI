0ddfafaf2dc949c5e2049191ea5894707b17ce33
enslaved2die <sellke.joshua@gmail.com>
2018-07-06 15:51:20 +0200

    Alert Activation Toggles added

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Text;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
+#region JSONHelper
 class JsonHelper
 {
     private const string INDENT_STRING = "    ";
@@ -78,15 +80,17 @@ static class Extensions
         }
     }
 }
+#endregion
 
 public class GameManager : MonoBehaviour
 {
+    #region VARIABLES
     public TMP_InputField apiKeyField;
 
     [Header("API Settings")]
     public string API_KEY;
     public const string URL = "https://api.tipeeestream.com/v1.0/";
-    public int limit;
+    public int limit = 4;
     public float UpdateRate = 1;
     float TimeHolder;
 
@@ -94,13 +98,25 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI FollowerCount;
     public TextMeshProUGUI SubscriptionCount, DonationAmount, CheerAmount, YTSubAmount;
     public TextMeshProUGUI responseText, lastEventIDField;
+    [Space]
+    public Toggle followToggle;
+    public Toggle subsToggle, donationToggle, hostsToggle;
+    public bool subsActive, followsActive, donationsActive, hostsActive;
+    private bool initialized = false;
 
     [Header("Alerts")]
     public Follower follower;
     public Subscription subscription;
     public Donation donation;
     public Host host;
+    [Space]
     public GameObject saveArea;
+    private TextMeshProUGUI ErrorText;
+    [Header("Last Events")]
+    public GameObject lastEventsPrefab;
+    public RectTransform EventsLayoutGroup;
+    [Space]
+    public List<LastEvent> lastEvents;
 
     [Header("Requested Data")]
     public RootObject foreverData;
@@ -108,34 +124,47 @@ public class GameManager : MonoBehaviour
     private int lastEventID = 0;
 
     [System.Serializable]
+    public struct LastEvent
+    {
+        public GameObject Holder;
+        public TextMeshProUGUI User;
+        public TextMeshProUGUI Type;
+        public TextMeshProUGUI Amount;
+        public TextMeshProUGUI Message;
+    }
+    [System.Serializable]
     public struct Follower
     {
         public GameObject Root;
-        public TextMeshPro User;
+        public TextMeshProUGUI User;
+        public GameObject _3DLayer;
     };
     [System.Serializable]
     public struct Subscription
     {
         public GameObject Root;
-        public TextMeshPro User;
-        public TextMeshPro Message;
-        public TextMeshPro Month;
+        public TextMeshProUGUI User;
+        public TextMeshProUGUI Message;
+        public TextMeshProUGUI ResubCount;
+        public GameObject _3DLayer;
     };
     [System.Serializable]
     public struct Donation
     {
         public GameObject Root;
-        public TextMeshPro User;
-        public TextMeshPro Message;
-        public TextMeshPro Amount;
+        public TextMeshProUGUI User;
+        public TextMeshProUGUI Message;
+        public TextMeshProUGUI Amount;
+        public GameObject _3DLayer;
     };
     [System.Serializable]
     public struct Host
     {
         public GameObject Root;
-        public TextMeshPro User;
+        public TextMeshProUGUI User;
+        public GameObject _3DLayer;
     };
-
+    #endregion
 
     #region General and Request
     [ContextMenu("Clear API Key")]
@@ -148,23 +177,68 @@ public class GameManager : MonoBehaviour
     {
         PlayerPrefs.DeleteKey("lastEventID");
     }
+    [ContextMenu("Clear Alert Bools")]
+    public void ClearAlertsBools()
+    {
+        PlayerPrefs.DeleteKey("subsBool");
+        PlayerPrefs.DeleteKey("donationsBool");
+        PlayerPrefs.DeleteKey("followsBool");
+        PlayerPrefs.DeleteKey("hostsBool");
+    }
     void Awake()
     {
+        ErrorText = saveArea.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
         API_KEY = PlayerPrefs.GetString("API_KEY");
         lastEventID = PlayerPrefs.GetInt("lastEventID");
         apiKeyField.text = API_KEY;
         lastEventIDField.text = lastEventID.ToString();
-        saveArea.SetActive(false);
+
+        #region SET TOGGGLES
+
+        if (PlayerPrefs.GetInt("subsBool") == 1)
+            subsActive = true;
+        else
+            subsActive = false;
+
+        if (PlayerPrefs.GetInt("donationsBool") == 1)
+            donationsActive = true;
+        else
+            donationsActive = false;
+
+        if (PlayerPrefs.GetInt("followsBool") == 1)
+            followsActive = true;
+        else
+            followsActive = false;
+
+        if (PlayerPrefs.GetInt("hostsBool") == 1)
+            hostsActive = true;
+        else
+            hostsActive = false;
+
+        subsToggle.isOn = subsActive;
+        followToggle.isOn = followsActive;
+        donationToggle.isOn = donationsActive;
+        hostsToggle.isOn = hostsActive;
+
+        initialized = true;
+
+        #endregion
+
     }
     private void Start()
     {
+
         if (!string.IsNullOrEmpty(API_KEY))
         {
             Request();
         }
 
         else
+        {
             Debug.LogWarning("NO API_KEY SET");
+            ErrorText.text = "NO API_KEY SET";
+        }
+
     }
     public void SetApiKey()
     {
@@ -200,6 +274,7 @@ public class GameManager : MonoBehaviour
     }
     public void Request()
     {
+        saveArea.SetActive(false);
         Debug.Log("Request");
         WWW eventRequest = new WWW(URL + "events.json?apiKey=" + API_KEY + "&type[]=follow&type[]=donation&type[]=subscription&limit=" + limit);
         WWW foreverRequest = new WWW(URL + "events/forever.json?apiKey=" + API_KEY);
@@ -217,6 +292,9 @@ public class GameManager : MonoBehaviour
         yield return req;
         text.text = JsonHelper.FormatJson(req.text);
         eventData = JsonUtility.FromJson<RootObject>(req.text);
+
+
+
         if (eventData.datas.items[0].id != lastEventID)
         {
             lastEventID = eventData.datas.items[0].id;
@@ -224,24 +302,115 @@ public class GameManager : MonoBehaviour
             PlayerPrefs.SetInt("lastEventID", lastEventID);
             PlayerPrefs.Save();
 
-            if (eventData.datas.items[0].type.Equals("subscription"))
+
+
+            if (eventData.datas.items[0].type.Equals("subscription") && subsActive == true)
             {
                 ActivateAlert(alertState.subs);
+
+                if (lastEvents.Count > 4)
+                {
+                    Destroy(lastEvents.First<LastEvent>().Holder);
+                    lastEvents.RemoveAt(0);
+                }
+
+                LastEvent last = new LastEvent();
+
+                last.Holder = Instantiate(lastEventsPrefab, EventsLayoutGroup);
+                last.Holder.GetComponent<RectTransform>().SetAsFirstSibling();
+
+                last.User = last.Holder.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+                last.User.text = eventData.datas.items[0].parameters.username;
+                last.Type = last.Holder.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+                last.Type.text = eventData.datas.items[0].type.ToUpper();
+                last.Amount = last.Holder.transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+                last.Amount.text = eventData.datas.items[0].parameters.amount.ToString() + eventData.datas.items[0].parameters.currency;
+                last.Message = last.Holder.transform.GetChild(3).GetComponent<TextMeshProUGUI>();
+                last.Message.text = eventData.datas.items[0].parameters.message;
+
+                lastEvents.Add(last);
+
             }
 
-            if (eventData.datas.items[0].type.Equals("follow"))
+            if (eventData.datas.items[0].type.Equals("follow") && followsActive == true)
             {
                 ActivateAlert(alertState.follow);
+
+                if (lastEvents.Count > 4)
+                {
+                    Destroy(lastEvents.First<LastEvent>().Holder);
+                    lastEvents.RemoveAt(0);
+                }
+
+                LastEvent last = new LastEvent();
+
+                last.Holder = Instantiate(lastEventsPrefab, EventsLayoutGroup);
+                last.Holder.GetComponent<RectTransform>().SetAsFirstSibling();
+
+                last.User = last.Holder.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+                last.User.text = eventData.datas.items[0].parameters.username;
+                last.Type = last.Holder.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+                last.Type.text = eventData.datas.items[0].type.ToUpper();
+                last.Amount = last.Holder.transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+                last.Amount.text = eventData.datas.items[0].parameters.amount.ToString() + eventData.datas.items[0].parameters.currency;
+                last.Message = last.Holder.transform.GetChild(3).GetComponent<TextMeshProUGUI>();
+                last.Message.text = eventData.datas.items[0].parameters.message;
+
+                lastEvents.Add(last);
             }
 
-            if (eventData.datas.items[0].type.Equals("donation"))
+            if (eventData.datas.items[0].type.Equals("donation") && donationsActive == true)
             {
                 ActivateAlert(alertState.donation);
+
+                if (lastEvents.Count > 4)
+                {
+                    Destroy(lastEvents.First<LastEvent>().Holder);
+                    lastEvents.RemoveAt(0);
+                }
+
+                LastEvent last = new LastEvent();
+
+                last.Holder = Instantiate(lastEventsPrefab, EventsLayoutGroup);
+                last.Holder.GetComponent<RectTransform>().SetAsFirstSibling();
+
+                last.User = last.Holder.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+                last.User.text = eventData.datas.items[0].parameters.username;
+                last.Type = last.Holder.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+                last.Type.text = eventData.datas.items[0].type.ToUpper();
+                last.Amount = last.Holder.transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+                last.Amount.text = eventData.datas.items[0].parameters.amount.ToString() + eventData.datas.items[0].parameters.currency;
+                last.Message = last.Holder.transform.GetChild(3).GetComponent<TextMeshProUGUI>();
+                last.Message.text = eventData.datas.items[0].parameters.message;
+
+                lastEvents.Add(last);
             }
 
-            if (eventData.datas.items[0].type.Equals("hosting"))
+            if (eventData.datas.items[0].type.Equals("hosting") && hostsActive == true)
             {
                 ActivateAlert(alertState.host);
+
+                if (lastEvents.Count > 4)
+                {
+                    Destroy(lastEvents.First<LastEvent>().Holder);
+                    lastEvents.RemoveAt(0);
+                }
+
+                LastEvent last = new LastEvent();
+
+                last.Holder = Instantiate(lastEventsPrefab, EventsLayoutGroup);
+                last.Holder.GetComponent<RectTransform>().SetAsFirstSibling();
+
+                last.User = last.Holder.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+                last.User.text = eventData.datas.items[0].parameters.username;
+                last.Type = last.Holder.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+                last.Type.text = eventData.datas.items[0].type.ToUpper();
+                last.Amount = last.Holder.transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+                last.Amount.text = eventData.datas.items[0].parameters.amount.ToString() + eventData.datas.items[0].parameters.currency;
+                last.Message = last.Holder.transform.GetChild(3).GetComponent<TextMeshProUGUI>();
+                last.Message.text = eventData.datas.items[0].parameters.message;
+
+                lastEvents.Add(last);
             }
         }
 
@@ -249,33 +418,6 @@ public class GameManager : MonoBehaviour
     #endregion
 
     #region AlertStates
-
-    //void SubscriptionAlert()
-    //{
-    //    subscription.User.text = eventData.datas.items[0].parameters.username;
-    //    subscription.Message.text = eventData.datas.items[0].parameters.message;
-    //    Debug.Log("Subscription");
-    //}
-
-    //void DonationAlert()
-    //{
-    //    donation.User.text = eventData.datas.items[0].parameters.username;
-    //    donation.Message.text = eventData.datas.items[0].parameters.message;
-    //    Debug.Log("Donation");
-    //}
-
-    //void FollowerAlert()
-    //{
-    //    follower.User.text = eventData.datas.items[0].parameters.username;
-    //    Debug.Log("Follower");
-    //}
-
-    //void HostAlert()
-    //{
-    //    host.User.text = eventData.datas.items[0].parameters.username;
-    //    Debug.Log("Host");
-    //}
-
     public enum alertState { subs, follow, donation, host }
 
     void ActivateAlert(alertState alertState)
@@ -288,9 +430,14 @@ public class GameManager : MonoBehaviour
                 follower.Root.SetActive(false);
                 donation.Root.SetActive(false);
                 host.Root.SetActive(false);
+                subscription._3DLayer.SetActive(true);
+                follower._3DLayer.SetActive(false);
+                donation._3DLayer.SetActive(false);
+                host._3DLayer.SetActive(false);
 
                 subscription.User.text = eventData.datas.items[0].parameters.username;
                 subscription.Message.text = eventData.datas.items[0].parameters.message;
+                subscription.ResubCount.text = "Resub " + eventData.datas.items[0].parameters.resub + "x";
                 Debug.Log("Subscription");
                 break;
             case alertState.follow:
@@ -299,6 +446,10 @@ public class GameManager : MonoBehaviour
                 follower.Root.SetActive(true);
                 donation.Root.SetActive(false);
                 host.Root.SetActive(false);
+                subscription._3DLayer.SetActive(false);
+                follower._3DLayer.SetActive(true);
+                donation._3DLayer.SetActive(false);
+                host._3DLayer.SetActive(false);
 
                 follower.User.text = eventData.datas.items[0].parameters.username;
                 Debug.Log("Follower");
@@ -309,8 +460,13 @@ public class GameManager : MonoBehaviour
                 follower.Root.SetActive(false);
                 donation.Root.SetActive(true);
                 host.Root.SetActive(false);
+                subscription._3DLayer.SetActive(false);
+                follower._3DLayer.SetActive(false);
+                donation._3DLayer.SetActive(true);
+                host._3DLayer.SetActive(false);
 
                 donation.User.text = eventData.datas.items[0].parameters.username;
+                donation.Amount.text = eventData.datas.items[0].parameters.amount + eventData.datas.items[0].parameters.currency;
                 donation.Message.text = eventData.datas.items[0].parameters.message;
                 Debug.Log("Donation");
                 break;
@@ -320,6 +476,10 @@ public class GameManager : MonoBehaviour
                 follower.Root.SetActive(false);
                 donation.Root.SetActive(false);
                 host.Root.SetActive(true);
+                subscription._3DLayer.SetActive(false);
+                follower._3DLayer.SetActive(false);
+                donation._3DLayer.SetActive(false);
+                host._3DLayer.SetActive(true);
 
                 host.User.text = eventData.datas.items[0].parameters.username;
                 Debug.Log("Host");
@@ -329,6 +489,29 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public void SwitchSubsToggle(bool newValue)
+    {
+        subsActive = newValue;
+        PlayerPrefs.SetInt("subsBool", subsActive ? 1 : 0);
+    }
+
+    public void SwitchFollowToggle(bool newValue)
+    {
+        followsActive = newValue;
+        PlayerPrefs.SetInt("followsBool", followsActive ? 1 : 0);
+    }
+
+    public void SwitchDonationsToggle(bool newValue)
+    {
+        donationsActive = newValue;
+        PlayerPrefs.SetInt("donationsBool", donationsActive ? 1 : 0);
+    }
+
+    public void SwitchHostsToggle(bool newValue)
+    {
+        hostsActive = newValue;
+        PlayerPrefs.SetInt("hostsBool", hostsActive ? 1 : 0);
+    }
     #endregion
 
     #region Json Structure
